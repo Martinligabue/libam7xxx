96287291d6d9e1e7a421d4b5663e6e8d32699a6d
Antonio Ospite <ao2@ao2.it>
2016-01-09 13:17:49 +0100

    am7xxx-modeswitch: adjust comment formatting

--- examples/am7xxx-modeswitch.c
+++ examples/am7xxx-modeswitch.c
@@ -88,7 +88,8 @@ int main(void)
 		goto out_libusb_close;
 	}
 
-	/* Checking that the configuration has not changed, as suggested in
+	/* 
+	 * Checking that the configuration has not changed, as suggested in
 	 * http://libusb.sourceforge.net/api-1.0/caveats.html
 	 */
 	current_configuration = -1;
